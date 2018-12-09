fd13641aed7db6032dfcd726971bc58a4333c641
Annie Beltran <Anniebeltran23@gmail.com>
2018-12-08 22:23:39 -0800

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Pelon',
+		photo: 'https://i.pinimg.com/originals/43/69/26/436926dc57adf598bc8f85f141337990.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Angel Girl',
+		photo: 'https://i.pinimg.com/originals/b0/ab/40/b0ab404b5785e0702611e880a2e09a9b.png',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'El Paletero',
+		photo: 'https://i.pinimg.com/originals/26/68/cc/2668cce9a15d2b0145baa51f41a86c79.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Eight Ball',
+		photo: 'https://i.pinimg.com/originals/18/be/90/18be90765d199bf14de267bbc9e861e7.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Fly Girl',
+		photo: 'https://i.pinimg.com/originals/d9/3c/9b/d93c9b8faf2b2a2fb51aa1974e5c174a.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Mr. Raza',
+		photo: 'https://i.pinimg.com/originals/6e/54/c2/6e54c209cf8e78b7dcbe4654c6b69f43.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Joker',
+		photo: 'https://i.pinimg.com/originals/b0/b9/02/b0b902a3817773b986860c7950d8655f.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Right Eye',
+		photo: 'https://i.pinimg.com/originals/fe/46/07/fe460741a81cc794729e38add50d2d9d.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'White Boy',
+		photo: 'https://i.pinimg.com/originals/2b/5b/3e/2b5b3e1778a26cac6575c8e55aeee2f0.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Tiny',
+		photo: 'https://i.pinimg.com/originals/b1/6c/54/b16c54d72370675e177876ffec6d3437.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
